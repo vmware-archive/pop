7d78e1ab353794d4c0bc93743430b65765174f33
Thomas S Hatch <thatch45@gmail.com>
2019-02-28 12:54:21 -0700

    Add the dirs conf plugin

---
+++ pop/mods/conf/dirs.py
@@ -0,0 +1,43 @@
+'''
+Used to take care of the options that end in `_dir`. The assumption is that
+`_dir` options need to be treated differently. They need to verified to exist
+and they need to be rooted based on the user, root option etc.
+'''
+# Import python libs
+import os
+
+
+def roots(hub, opts, cdir):
+    '''
+    Detect the root dir data and apply it
+    '''
+    # TODO: Make this safe for Windows
+    os_root = '/'
+    root = os_root
+    change = False
+    if not os.geteuid() == 0:
+        root = os.path.join(os.environ['HOME'], cdir)
+        change = True
+    if opts.get('root_dir', root) != root:
+        root = opts['root_dir']
+        change = True
+    if change:
+        for key in opts:
+            if key == 'root_dir':
+                continue
+            if key.endswith('_dir'):
+                opts[key] = opts[key].replace(
+                    os_root, root, 1)
+
+
+def verify(hub, opts):
+    '''
+    Verify that the environment and all named directories in the
+    configuration exist
+    '''
+    for key in opts:
+        if key == 'root_dir':
+            continue
+        if key.endswith('_dir'):
+            if not os.path.isdir(opts[key]):
+                os.makedirs(opts[key])

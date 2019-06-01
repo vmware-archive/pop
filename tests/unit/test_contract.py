16117e827b9e5c2bbf9de7de8b7f97e9826d45b1
David Hilton <david.hilton.p@gmail.com>
2019-05-31 18:55:51 -0600

    pre-calculate contract_functions for Contracted

---
+++ tests/unit/test_contract.py
@@ -0,0 +1,11 @@
+from pop.contract import Contracted
+
+
+def test_contracted_shortcut():
+    def f(hub):
+        pass
+
+    c = Contracted(parent=None, contracts=[], func=f)
+    c.contract_functions['pre'] = [None]  # add some garbage so we raise if we try to evaluate contracts
+
+    c()

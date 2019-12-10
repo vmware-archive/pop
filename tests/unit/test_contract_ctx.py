f5c8975955b898db0f1170263c2342f74651c923
David Hilton <david.hilton.p@gmail.com>
2019-12-10 12:44:55 -0500

    take args from contract_context instead of originals in direct call case, too

--- tests/unit/test_contract_ctx.py
+++ tests/unit/test_contract_ctx.py
@@ -17,16 +17,29 @@ def test_contract_context():
     assert hub.mods.ctx.test() == 'contract executed'
 
 
-def test_contract_context_update():
+def test_contract_context_update_call():
+    # if a pre modifies args, make sure they persist when called via 'call' function
     hub = pop.hub.Hub()
     hub.pop.sub.add(
             pypath='tests.mods.contract_ctx',
             subname='mods',
             contracts_pypath='tests.contracts'
             )
-    assert hub.mods.ctx_update.test(True) == 'contract executed'
+    assert hub.mods.ctx_update.test_call(True) == 'contract executed'
     # Multiple calls have the same outcome
-    assert hub.mods.ctx_update.test(True) == 'contract executed'
+    assert hub.mods.ctx_update.test_call(True) == 'contract executed'
+
+
+def test_contract_context_update_direct():
+    # if a pre modifies args, make sure they persist when called directly
+    hub = pop.hub.Hub()
+    hub.pop.sub.add(
+            pypath='tests.mods.contract_ctx',
+            subname='mods',
+            contracts_pypath='tests.contracts'
+            )
+    assert hub.mods.ctx_update.test_direct(True) is False
+    assert hub.mods.ctx_update.test_direct(True) is False
 
 
 def test_contract_ctx_argument_retrieval():
